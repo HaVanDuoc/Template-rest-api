37de0b79d869b33fc6f7786db77f9458c0c6877b
Hà Văn Được <havanduoc.dev@gmail.com>
2022-08-13 16:00:52 +0700

    Connecting MongoDB

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const morgan = require('morgan')
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGODB_URL, () => {
+    console.log("Connected to MongoDB")
+});
+
 app.listen(1810, () => {
     console.log("Server is running!")
 })
